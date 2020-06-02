44ef8ef389b974f5a415a01ae37f7b5299c657a9
Gabriel Paladines–Primary –gabriel.paladines@gaphit.com <gabriel.paladines@gaphit.com>
2020-06-02 13:28:57 -0400

    Actualizacion preguntas 4.1

--- code/PRA2.R
+++ code/PRA2.R
@@ -32,12 +32,15 @@ colSums(d_suicides=="0")
 
 # Análisis mediante diagramas de cajas e histogramas
 par(mfrow=c(2,2))
-for(i in 1:ncol(d_suicides)) {
+cols <- c("suicides_no","population")
+for(i in cols) {
   if (is.integer(d_suicides[,i])){
-    boxplot(d_suicides[,i], main = colnames(d_suicides)[i], width = 100)
-    hist(d_suicides[,i], main = colnames(d_suicides)[i])
+    boxplot(d_suicides[,i], main = i, width = 100)
+    hist(d_suicides[,i], main = i)
   }
 }
+
+# Por la naturaleza de las variables population y suicides, no siguen una distribución normal.
 
 par(mfrow=c(2,2))
 cols <- c("sex","age","generation")
@@ -49,15 +52,50 @@ for (i in cols){
   }
 }
 
+# La variable sex tiene dos clases: female y male, ambas con la misma cantidad de registros
+# La variable age tiene 6 clases: "", ambas con la misma cantidad de registros.
+# La variable generation tiene 6 clases con diferente cantidad de registros.
+
 
 # 4 ANALISIS DE LOS DATOS
 
 # 4.1 Seleccion de los grupos de datos que se quieren analizar
 
+# ¿Es mayor la tasa de suicidios en hombres que en mujeres (en función del porcentaje de población)?
+#  - Atributos: sex, suicides_no, population
+#  - Calcular la tasa de suicidios por sex.
+#  - Gráfica o tabla donde se muestra una comparativa entre la tasa de suicidios entre hombres y mujeres.
+
+
+
+#¿Qué generación tiene una tasa más alta de suicidios según el nivel de riqueza de su país en el 2008 durante la crisis económica mundial?
+  
+#  -  Atributos: suicides_no, population, generation, gdp_per_capita...., country, year
+#  -	Clasificar los países según el producto interno bruto gdp_for_capita para el 2008.
+#  -	Agregar una columna nueva con la clasificación en 3 grupos (Primer mundo, segundo mundo, tercer mundo), para esto utilizaremos un algoritmo de clasificación no supervisado.
+#  -  Calcular la tasa de suicidios por generación / nivel de riqueza.
+#  -	Comparativa por generación y tasa de suicidios para el año 2008.
+
+
+#¿Cómo ha evolucionado la tasa de suicidios desde el 1985 en España? 
+
+# - Atributos: suicides_no, population, year, country=Spain,  
+# -	Columna que sume el número por cada año (actualmente por cada anio existen 12 registros, 2 sex x 6 age).
+#-	Columna que sume la población por cada año.
+#-	Calcular la tasa de suicidios por cada año.
+#-	Graficar para identificar de picos o valores altos.
+
+# Proyección del número de suicidios en los siguientes 5 años.
+
+# Nota: Pregunta derivada en función de los resultados de la pregunta anterior.
+
 
 # 4.2 Comprobacion de la normalidad y homogeneidad de la varianza
+
+# Análisis de Normalidad
+
 par(mfrow=c(2,2))
-cols <- c("suicides_no", "population", "suicides.100k.pop", "gdp_per_capita....")
+cols <- c("suicides_no", "population")
 for (i in cols){
   qqnorm(d_suicides[,i],main = paste("Normal Q-Q Plot for ", i))
   qqline(d_suicides[,i],col="red")
@@ -65,5 +103,9 @@ for (i in cols){
         main=paste("Histogram for ", i), 
         xlab=i, freq = FALSE)
 }
+ # No son variables normales...
+
+# Análisis de homogeneidad de la varianza
 
 # 4.3 Aplicando pruebas estadisticas para comparar grupos
+
