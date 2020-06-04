483d461e37d2f34ba6f374023509f383d2608fd9
JaimePardo27 <62807578+JaimePardo27@users.noreply.github.com>
2020-06-04 19:51:02 +0200

    Add files via upload

--- code/PRA2.R
+++ code/PRA2.R
@@ -3,11 +3,12 @@
 library(dplyr)
 library(knitr)
 library(ggplot2)
+library(stats)
 
 # 1 DESCRIPCION DEL DATASET
 
 # Lectura de datos
-data <- read.csv("../data/suicide.csv", header = TRUE, sep = ",", quote="\"", dec=".",fill = TRUE)
+data <- read.csv("suicide.csv", header = TRUE, sep = ",", quote="\"", dec=".",fill = TRUE)
 dim(data) 
 # Tenemos 27820 observaciones y un total de 12 variables.
 summary(data)
@@ -22,16 +23,16 @@ kable(data.frame(variables=names(res),clase=as.vector(res)))
 d_suicides <- select(data, -HDI.for.year, -country.year)
 str(d_suicides)
 
-# 3 LIMIPIEZA DE DATOS
+# 3 LIMPIEZA DE DATOS
 
-# 3.1 Los datos que contienen ceros o elementos vacÃ­os
+# 3.1 Los datos que contienen ceros o elementos vacíos
 colSums(is.na(d_suicides))
 colSums(d_suicides=="")
 colSums(d_suicides=="0")
 
-# 3.2 IdentificaciÃ³n y tratamiento de valores extremos
+# 3.2 Identificación y tratamiento de valores extremos
 
-# AnÃ¡lisis mediante diagramas de cajas e histogramas
+# Análisis mediante diagramas de cajas e histogramas
 par(mfrow=c(2,2))
 cols <- c("suicides_no","population")
 for(i in cols) {
@@ -41,7 +42,7 @@ for(i in cols) {
   }
 }
 
-# Por la naturaleza de las variables population y suicides, no siguen una distribuciÃ³n normal.
+# Por la naturaleza de las variables population y suicides, no siguen una distribución normal.
 
 par(mfrow=c(2,2))
 cols <- c("sex","age","generation")
@@ -62,38 +63,136 @@ for (i in cols){
 
 # 4.1 Seleccion de los grupos de datos que se quieren analizar
 
-# Â¿Es mayor la tasa de suicidios en hombres que en mujeres (en funciÃ³n del porcentaje de poblaciÃ³n)?
+# ¿Es mayor la tasa de suicidios en hombres que en mujeres (en función del porcentaje de población)?
 #  - Atributos: sex, suicides_no, population
 #  - Calcular la tasa de suicidios por sex.
-#  - GrÃ¡fica o tabla donde se muestra una comparativa entre la tasa de suicidios entre hombres y mujeres.
+#  - Gráfica o tabla donde se muestra una comparativa entre la tasa de suicidios entre hombres y mujeres.
 
 
+d_suicides_male <- filter(d_suicides, sex=='male')
+d_suicides_female <- filter(d_suicides, sex=='female')
 
-#Â¿QuÃ© generaciÃ³n tiene una tasa mÃ¡s alta de suicidios segÃºn el nivel de riqueza de su paÃ­s en el 2008 durante la crisis econÃ³mica mundial?
-  
-#  -  Atributos: suicides_no, population, generation, gdp_per_capita...., country, year
-#  -	Clasificar los paÃ­ses segÃºn el producto interno bruto gdp_for_capita para el 2008.
-#  -	Agregar una columna nueva con la clasificaciÃ³n en 3 grupos (Primer mundo, segundo mundo, tercer mundo), para esto utilizaremos un algoritmo de clasificaciÃ³n no supervisado.
-#  -  Calcular la tasa de suicidios por generaciÃ³n / nivel de riqueza.
-#  -	Comparativa por generaciÃ³n y tasa de suicidios para el aÃ±o 2008.
+total_suicides_male = colSums (select (d_suicides_male, contains ("suicides_no")))
+total_suicides_female = colSums (select (d_suicides_female, contains ("suicides_no")))
+
+# Total suicidios hombres y mujeres respectivamente:
+total_suicides_male
+total_suicides_female
+
+total_pop_male = colSums (select (d_suicides_male, contains ("population")))
+total_pop_female = colSums (select (d_suicides_female, contains ("population")))
+
+# Total población hombres y mujeres respectivamente:
+total_pop_male
+total_pop_female
+
+rate_male = total_suicides_male*100000 / total_pop_male
+rate_female = total_suicides_female*100000 / total_pop_female
+
+# Tasa suicidios hombres y mujeres respectivamente:
+rate_male
+rate_female
+
+percent_male = rate_male / (rate_male + rate_female)
+percent_female = rate_female / (rate_male + rate_female)
+
+# Porcentaje suicidios hombres y mujeres respectivamente:
+percent_male
+percent_female
+
+# Conclusión: la tasa de suicidios en hombres es mayor que en mujeres, más del triple (20.7 vs. 5.94)
+# En total representan el 77,7% de los suicidios
 
 
-#Â¿CÃ³mo ha evolucionado la tasa de suicidios desde el 1985 en EspaÃ±a? 
+#¿Qué generación tiene una tasa más alta de suicidios según el nivel de riqueza de su país en el 2008 durante la crisis económica mundial?
+
+
+#  -	Clasificar los países según el producto interno bruto gdp_for_capita para el 2008.
+#  -  Calcular la tasa de suicidios por generación / nivel de riqueza.
+#  -	Comparativa por generación y tasa de suicidios para el año 2008.
+
+# Empezamos clasificando los países en 3 niveles de riqueza, mediante el algoritmo k-means.
+# Se genera la columna nivel_riqueza (1=Segundo mundo; 2=Tercer; 3=Primer)
+
+# Atributos: suicides_no, population, generation, gdp_per_capita...., country, year
+
+suicides_2008 <- filter(d_suicides, year=='2008')
+
+modelo <- kmeans(suicides_2008$gdp_per_capita...., centers = 3)
+modelo
+modelo$cluster
+plot(modelo$cluster)
+
+suicides_2008$nivel_riqueza <- modelo$cluster
+suicides_2008
+par(mfrow=c(2,2))
+suicides_2008_riqueza <- filter(suicides_2008, nivel_riqueza==1)
+df  <- suicides_2008_riqueza %>% select(generation, suicides_no, population)
+df[2:3] <- lapply(df[2:3], as.numeric)
+report <- df %>%
+  group_by(generation) %>%
+  summarise_all(funs(sum)) %>%
+  mutate(suicides_100k_pop = suicides_no / population * 100000)
+
+report
+
+g <- ggplot(report, aes(generation,suicides_100k_pop))
+g + geom_bar(stat="identity", width = 0.5, fill='darkblue') + 
+  labs(title=paste("SUICIDIOS 2008 NIVEL RIQUEZA ", 1) , 
+       subtitle="Tasa de suicidios por generacion") +
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+
+#¿Cómo ha evolucionado la tasa de suicidios desde el 1985 en España? 
 
 # - Atributos: suicides_no, population, year, country=Spain,  
-# -	Columna que sume el nÃºmero por cada aÃ±o (actualmente por cada anio existen 12 registros, 2 sex x 6 age).
-#-	Columna que sume la poblaciÃ³n por cada aÃ±o.
-#-	Calcular la tasa de suicidios por cada aÃ±o.
+# -	Columna que sume el número por cada año (actualmente por cada anio existen 12 registros, 2 sex x 6 age).
+#-	Columna que sume la población por cada año.
+#-	Calcular la tasa de suicidios por cada año.
 #-	Graficar para identificar de picos o valores altos.
 
-# ProyecciÃ³n del nÃºmero de suicidios en los siguientes 5 aÃ±os.
 
-# Nota: Pregunta derivada en funciÃ³n de los resultados de la pregunta anterior.
+df  <- d_suicides %>% select(sex, suicides_no, population)
+df[2:3] <- lapply(df[2:3], as.numeric)
+report <- df %>%
+  group_by(sex) %>%
+  summarise_all(funs(sum)) %>%
+  mutate(suicides_100k_pop = suicides_no / population * 100000)
+report
+theme_set(theme_classic())
+g <- ggplot(report, aes(sex,suicides_100k_pop))
+g + geom_bar(stat="identity", width = 0.5, fill="tomato2") + 
+  labs(title="Bar Chart", 
+       subtitle="Tasa de suicidios por genero") +
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+
+### Suicidios en España
+
+suicides_spain <- select(data, ï..country, year, suicides_no, population)
+suicides_spain <- filter(d_suicides, ï..country=='Spain')
+df  <- suicides_spain %>% select(year, suicides_no, population)
+df[2:3] <- lapply(df[2:3], as.numeric)
+report <- df %>%
+  group_by(year) %>%
+  summarise_all(funs(sum)) %>%
+  mutate(suicides_100k_pop = suicides_no / population * 100000)
+report
+
+theme_set(theme_classic())
+g <- ggplot(report, aes(year,suicides_100k_pop))
+g + geom_line(stat="identity", color="green") + 
+  labs(title="Bar Chart", 
+       subtitle="Tasa de suicidios por genero") +
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+
+
+# Proyección del número de suicidios en los siguientes 5 años.
+
+# Nota: Pregunta derivada en función de los resultados de la pregunta anterior.
 
 
 # 4.2 Comprobacion de la normalidad y homogeneidad de la varianza
 
-# AnÃ¡lisis de Normalidad
+# Análisis de Normalidad
 
 par(mfrow=c(2,2))
 cols <- c("suicides_no", "population")
@@ -101,32 +200,15 @@ for (i in cols){
   qqnorm(d_suicides[,i],main = paste("Normal Q-Q Plot for ", i))
   qqline(d_suicides[,i],col="red")
   hist(d_suicides[,i], 
-        main=paste("Histogram for ", i), 
-        xlab=i, freq = FALSE)
+       main=paste("Histogram for ", i), 
+       xlab=i, freq = FALSE)
 }
- # No son variables normales...
+# No son variables normales...
 
-# AnÃ¡lisis de homogeneidad de la varianza
+# Análisis de homogeneidad de la varianza
 
 # 4.3 Aplicando pruebas estadisticas para comparar grupos
 
 
-# Â¿Es mayor la tasa de suicidios en hombres que en mujeres (en funciÃ³n del porcentaje de poblaciÃ³n)?
-#  - Atributos: sex, suicides_no, population
-#  - Calcular la tasa de suicidios por sex.
-#  - GrÃ¡fica o tabla donde se muestra una comparativa entre la tasa de suicidios entre hombres y mujeres.
-df  <- d_suicides %>% select(sex, suicides_no, population)
-df[2:3] <- lapply(df[2:3], as.numeric)
-report <- df %>%
-  group_by(sex) %>%
-  summarise_all(funs(sum)) %>%
-  mutate(suicides_100k_pop = suicides_no / population * 100000)
-
-theme_set(theme_classic())
-g <- ggplot(report, aes(sex,suicides_100k_pop))
-g + geom_bar(stat="identity", width = 0.5, fill="tomato2") + 
-  labs(title="Bar Chart", 
-       subtitle="Tasa de suicidios por genero") +
-  theme(axis.text.x = element_text(angle=65, vjust=0.6))
 
 
